ac715ee077b464c66da2b03298b8bccdf7af2aa2
alisak419 <alisateo@gmail.com>
2025-01-02 13:24:22 +0200

    Initial commit

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from crypto.SelfTest.Protocol.test_ecdh import private_key
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 import json
@@ -8,3 +9,12 @@ import socket
 clients_data_base = {}
 
 #We will define a local IP address. The client and the server will operate on the same computer.
+HOST = "127.0.0.1"
+PORT = 12345
+
+#Generate a pair of public and private keys for the client with the RSA algorithm.
+private_key_of_client = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+#The public exponent, 65537, is a prime number and is a standart choice for public exponent.
+#The key size 2048 is a large key size and offers a good security.
+
+public_key_of_client = private_key_of_client.public_key()   #Derive the public key from the private key.
